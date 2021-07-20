22755f1f6054a2faafe9f82c55bfe159409f4f4e
kschao <koyschao@icloud.com>
2021-07-20 14:06:18 -0500

    updated codes on each files.

--- Game.py
+++ Game.py
@@ -1,23 +1,26 @@
-from ai import AI
-from human import Human
-from player import Player #Do we need the above two import or would we just import in the parent? (Player)
+from AI import AI
+# from human import Human
+from Player import Player #Do we need the above two import or would we just import in the parent? (Player)
 
-Class Game:
+class Game:
     def __init__(self):
-        self.player = Player() #Look at above comment. Would this be self.ai and self.human or just self.player?
+        self.player1 = AI()
+        self.player2 = None     #Look at above comment. Would this be self.ai and self.human or just self.player?
 
     def run_game(self):
-        self.display_welcome()
-        self.play() #?
-        self.display_winnter()
+        self.player1.choose_gesture()
+        print(self.player1.chosen_gesture)
+        # self.display_welcome()
+        # self.play() #?
+        # self.display_winnter()
 
-    def play(self):
-        # Will need to figure out this part
+    # def play(self):
+    #     # Will need to figure out this part
 
-    def display_winner(sel):
-        if (self.human.score > self.ai.score):
-            print "Human wins!"
-        elif (self.human.score < self.ai.score):
-            print "AI wins!"
-        else
-            print "It's a tie. Play again!"
+    # def display_winner(sel):
+    #     if (self.human.score > self.ai.score):
+    #         print "Human wins!"
+    #     elif (self.human.score < self.ai.score):
+    #         print "AI wins!"
+    #     else
+    #         print "It's a tie. Play again!"
---
+++ Main.py
@@ -0,0 +1,4 @@
+from Game import Game
+
+game_one = Game()
+game_one.run_game()
