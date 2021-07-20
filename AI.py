a4febc8e2a55948e76a541fdd7d0df4fd60c95e4
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-20 16:21:33 -0500

    Took out note in AI.py file

--- AI.py
+++ AI.py
@@ -5,6 +5,6 @@ class AI(Player):
     def __init__(self):
         super().__init__('AI')
 
-    def choose_gesture(self): #I know this is wrong but something like that?
+    def choose_gesture(self):
         self.chosen_gesture = random.choice(self.gestures_list)
 
