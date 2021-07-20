076e37940b567a289e2341989e14ab48ae85d250
kschao <koyschao@icloud.com>
2021-07-20 18:19:14 -0500

    fixed indentation on player line 2

--- Player.py
+++ Player.py
@@ -1,12 +1,12 @@
 class Player:
-    def __init__(self,name):
+  def __init__(self,name):
         self.name = name
         self.gestures_list = []
         self.score = 0
         self.chosen_gesture = None
 
 
-    def choose_gesture(self):
+  def choose_gesture(self):
      gesture_choice = input("choose a gesture: (rock[1], paper[2], scissors[3],lizard[4],spock[5]): ")
      if gesture_choice == "1": 
        print("you have chosen rock")
