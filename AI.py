086ab78ad235eb441b61e1682f514ce796f36542
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-07-20 09:47:28 -0500

    blah

---
+++ AI.py
@@ -0,0 +1,10 @@
+from player import Player
+
+class AI(Player):
+    def __init__(self):
+
+    def chooseGesture: #I know this is wrong but something like that?
+        pass
+
+    def gesturerandomizer: #Will need to finish this
+        
