17b7808ee32a90b8bbe9e1f095283232648a0dd1
unknown <kaciehawkins1@gmail.com>
2021-07-20 15:10:23 -0600

    added method to choose gesture in player class

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 from Game import Game
+from Player import Player
 
 game_one = Game()
 game_one.run_game()
+
+player1 = Player("fred")
+ 
+player1.choose_gesture()
--- Player.py
+++ Player.py
@@ -1,14 +1,28 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self,name):
         self.name = name
-        self.gestures_list = ['rock', 'scissor', 'paper', 'lizard', 'spock']
+        self.gestures_list = []
         self.score = 0
         self.chosen_gesture = None
 
 
     def choose_gesture(self):
-        pass
-    
+     gesture_choice = input("choose a gesture: (rock[1], paper[2], scissors[3],lizard[4],spock[5]): ")
+     if gesture_choice == "1": 
+       print("you have chosen rock")
+     elif gesture_choice == "2":
+       print("you have chosen paper")
+     elif gesture_choice == "3":
+       print("you have chosen scissor")
+     elif gesture_choice == "4":
+       print ("you have chosen lizard")
+     elif gesture_choice == "5":
+      print("you have chosen spock")
+     return gesture_choice
+      
+
+
+
 
 
 
